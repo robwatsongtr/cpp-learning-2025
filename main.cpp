75d52fa346aa0b92e71e4b1ffda1dc8b43cbae22
Robert Diamond <rmd6502@gmail.com>
2025-03-29 16:57:11 -0700

    create smart pointer code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "templates.h"
 #include "raw_pointers.h"
 #include "smart_pointers.h"
-
+#include <memory>
 
 int main() {
 
@@ -9,12 +9,16 @@ int main() {
 
 
 
-    // template class code 
-    mynamespace::MyTemplate<int> obj(42);  // int spec 
-    obj.display();
+    // template class code
+    // mynamespace::MyTemplate<int> obj(42);  // int spec
+    // obj.display();
 
-    mynamespace::MyTemplate<std::string> strObj("Hello, Templates!"); // string spec
-    strObj.display();
+    // mynamespace::MyTemplate<std::string> strObj("Hello, Templates!"); // string spec
+    // strObj.display();
+
+    SmartPointers ptrs;
+    std::shared_ptr<std::string> ptr = std::make_shared<std::string>("hello");
+    ptrs.make_pointers(ptr);
 
     return 0;
 }
--- smart_pointers.h
+++ smart_pointers.h
@@ -2,11 +2,15 @@
 #define SMARTPOINTERS_H
 
 #include <iostream>
+#include <memory>
+#include <string>
 
 class SmartPointers {
 public:
     SmartPointers();
     ~SmartPointers();
+
+    void make_pointers(std::shared_ptr<std::string> &of);
 
 private:
     int data;
